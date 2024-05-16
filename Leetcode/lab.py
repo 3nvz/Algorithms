734c34f3fb388834c4a3ea81022baadb8e200ae0
3nvz <rscherer.jobs@gmail.com>
2024-05-16 07:29:42 -0700

    leetcode stuff

--- Leetcode/lab.py
+++ Leetcode/lab.py
@@ -1,1 +1,21 @@
 
+# count up to n + 1
+# convert every count starting from 0 to binary
+# count the number of 1's in the binary result
+# append the number of 1's to the result array
+
+n = 5
+result = []
+result.append(0)
+
+
+for i in range(n + 1):
+    tmp = bin(i)
+    counter = 0
+    for char in tmp:
+        if char == '1':
+            counter += 1
+    if counter != 0:
+        result.append(counter)
+
+print(result)
