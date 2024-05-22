9099d9e58da8fa10d7317b40eebafe92f17543a1
3nvz <rscherer.jobs@gmail.com>
2024-05-22 09:38:29 -0700

    some react stuff

--- Leetcode/lab.py
+++ Leetcode/lab.py
@@ -2,7 +2,7 @@
 # find min
 # convert to tuple so it's order and removes duplicates
 # loop through every number from min to max and check if this number is in the tuple
-# if not add it to the result
+# if not add it to the resultc
 
 from typing import Counter
 
