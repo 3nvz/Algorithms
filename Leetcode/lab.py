77040608dbd3cad7a4187ce461c9b7bf3edc91d0
3nvz <rscherer.jobs@gmail.com>
2024-05-27 14:33:09 -0700

    implemented front end from full stack app

--- Leetcode/lab.py
+++ Leetcode/lab.py
@@ -4,7 +4,7 @@
 # loop through every number from min to max and check if this number is in the tuple
 # if not add it to the resultc
 
-# kept on working full stack app, logic is done and functioning
+# kept on working full stack app, designed front end and implemented that
 
 import os
 random_bytes = os.urandom(24)
