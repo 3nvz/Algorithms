82b6d91c363bfbc95289713d958951232e47574a
3nvz <rscherer.jobs@gmail.com>
2024-05-30 13:21:16 -0700

    c++ practice - linked list

--- Leetcode/lab.py
+++ Leetcode/lab.py
@@ -4,7 +4,7 @@
 # loop through every number from min to max and check if this number is in the tuple
 # if not add it to the resultc
 
-# kept on working full stack app, designed front end and implemented that
+# implemented singly linked list in c++
 
 import os
 random_bytes = os.urandom(24)
