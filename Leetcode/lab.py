e13174673f563c3ce7f2b9a563e10c475be2d993
3nvz <rscherer.jobs@gmail.com>
2024-06-02 11:45:09 -0700

    codechef again

--- Leetcode/lab.py
+++ Leetcode/lab.py
@@ -4,7 +4,7 @@
 # loop through every number from min to max and check if this number is in the tuple
 # if not add it to the resultc
 
-# codechef practice c++, hit 1100
+# codechef practice c++, hit 1100 - continued - going for 2 Star
 
 import os
 random_bytes = os.urandom(24)
