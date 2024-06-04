81d5955104ad4d3c001d39b1be9416f7fbcfdd3d
3nvz <rscherer.jobs@gmail.com>
2024-06-04 16:28:11 -0700

    continued with codechef

--- Leetcode/lab.py
+++ Leetcode/lab.py
@@ -4,8 +4,14 @@
 # loop through every number from min to max and check if this number is in the tuple
 # if not add it to the resultc
 
-# codechef practice c++, tackled 1400
+# codechef practice c++, tackled 1400 - continued today
 
+import hashlib
 import os
 random_bytes = os.urandom(24)
 print(random_bytes)
+username_trial = b"MORTON"
+
+print(hashlib.sha256(username_trial).hexdigest()[4])
+
+print("".join(hashlib.sha256(username_trial).hexdigest()[x] for x in (4, 5, 3, 6, 2, 7, 1, 8)))
